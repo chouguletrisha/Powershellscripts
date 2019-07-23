443c840b753beed3e7b71f44c7eaccab3a408f0c
Chougule <300524@cognizant.com>
2019-07-23 18:23:36 +0530

    archive script added

--- insert date in filename.ps1
+++ insert date in filename.ps1
@@ -1,4 +1,4 @@
 ﻿
- Get-ChildItem "C:\powershell\POWERSHELL\*.txt" | rename-item -NewName {$_.name -replace ".txt",((get-date).ToString("_yyyyMMdd_HHmmss")+".txt")}
+ Get-ChildItem "C:\Users\300524\Desktop\Powershellscripts\*.txt" | rename-item -NewName {$_.name -replace ".txt",((get-date).ToString("_yyyyMMdd_HHmmss")+".txt")}
   
  
